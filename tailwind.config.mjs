721856b6f2fea589afba4ade2a81845ffe9868b5
Simon <snsimonescobar@gmail.com>
2024-07-30 04:34:30 -0500

    second section added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+			colors: {
+				'main-green': '#56d07d'
+			}
 		},
 		fontFamily:{
 			'cronus-round': ['CronusRound', 'sans-serif']
